e693c224c80006fbf28a907ed3b89a0b9c3dd20d
Luca D'Amico <menteora@gmail.com>
2019-11-19 11:26:12 +0100

    fix: check if slots exist

--- src/components/CardPage/SimpleTitle.vue
+++ src/components/CardPage/SimpleTitle.vue
@@ -1,8 +1,9 @@
 <template>
   <div>
-    <h1 class="headline pa-3 font-weight-light"><slot name="outside"></slot></h1>
+    <h1 v-if="$slots['outside']" class="headline pa-3 font-weight-light"><slot name="outside"></slot></h1>
     <v-card class="mx-auto pa-xs-auto pa-sm-5 pa-md-8" :elevation="1">
-      <v-card-title class="font-weight-light"><slot name="inside"></slot></v-card-title>
+      <v-card-title v-if="$slots['inside']" class="font-weight-light"><slot name="inside"></slot></v-card-title>
+      <v-divider v-if="$slots['inside']" />
       <slot></slot>
     </v-card>
   </div>
